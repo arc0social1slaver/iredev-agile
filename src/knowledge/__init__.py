15d2e5094ee41d8a9daf89b10130b25810a5ebaa
arc0social1slaver <thanhphat2004vl@gmail.com>
2026-01-18 09:14:24 +0700

    chore: reformat file

--- src/knowledge/__init__.py
+++ src/knowledge/__init__.py
@@ -1,10 +1,20 @@
 # Knowledge management module for iReDev framework
 
 from .base_types import (
-    KnowledgeType, KnowledgeModule, KnowledgeMetadata, KnowledgeDependency,
-    KnowledgeModuleConfig, ModuleStatus, LoadPriority, KnowledgeQuery,
-    KnowledgeException, ModuleNotFoundException, ModuleLoadException,
-    DependencyException, ValidationException, VersionException
+    KnowledgeType,
+    KnowledgeModule,
+    KnowledgeMetadata,
+    KnowledgeDependency,
+    KnowledgeModuleConfig,
+    ModuleStatus,
+    LoadPriority,
+    KnowledgeQuery,
+    KnowledgeException,
+    ModuleNotFoundException,
+    ModuleLoadException,
+    DependencyException,
+    ValidationException,
+    VersionException,
 )
 
 from .knowledge_manager import KnowledgeManager
@@ -12,27 +22,50 @@ from .version_manager import KnowledgeVersionManager, KnowledgeUpdateManager
 from .dynamic_loader import DynamicKnowledgeLoader, LoaderConfig
 from .cot_engine import ChainOfThoughtEngine, CoTProcess, CoTStep, ReasoningStep
 from .loaders import (
-    BaseKnowledgeLoader, DomainKnowledgeLoader, MethodologyLoader,
-    StandardsLoader, TemplatesLoader, StrategiesLoader,
-    KnowledgeLoaderFactory, KnowledgeLoaderManager
+    BaseKnowledgeLoader,
+    DomainKnowledgeLoader,
+    MethodologyLoader,
+    StandardsLoader,
+    TemplatesLoader,
+    StrategiesLoader,
+    KnowledgeLoaderFactory,
+    KnowledgeLoaderManager,
 )
 
 __all__ = [
     # Base types
-    'KnowledgeType', 'KnowledgeModule', 'KnowledgeMetadata', 'KnowledgeDependency',
-    'KnowledgeModuleConfig', 'ModuleStatus', 'LoadPriority', 'KnowledgeQuery',
-    'KnowledgeException', 'ModuleNotFoundException', 'ModuleLoadException',
-    'DependencyException', 'ValidationException', 'VersionException',
-    
+    "KnowledgeType",
+    "KnowledgeModule",
+    "KnowledgeMetadata",
+    "KnowledgeDependency",
+    "KnowledgeModuleConfig",
+    "ModuleStatus",
+    "LoadPriority",
+    "KnowledgeQuery",
+    "KnowledgeException",
+    "ModuleNotFoundException",
+    "ModuleLoadException",
+    "DependencyException",
+    "ValidationException",
+    "VersionException",
     # Core managers
-    'KnowledgeManager', 'KnowledgeVersionManager', 'KnowledgeUpdateManager',
-    'DynamicKnowledgeLoader', 'LoaderConfig',
-    
+    "KnowledgeManager",
+    "KnowledgeVersionManager",
+    "KnowledgeUpdateManager",
+    "DynamicKnowledgeLoader",
+    "LoaderConfig",
     # Chain-of-Thought engine
-    'ChainOfThoughtEngine', 'CoTProcess', 'CoTStep', 'ReasoningStep',
-    
+    "ChainOfThoughtEngine",
+    "CoTProcess",
+    "CoTStep",
+    "ReasoningStep",
     # Loaders
-    'BaseKnowledgeLoader', 'DomainKnowledgeLoader', 'MethodologyLoader',
-    'StandardsLoader', 'TemplatesLoader', 'StrategiesLoader',
-    'KnowledgeLoaderFactory', 'KnowledgeLoaderManager'
+    "BaseKnowledgeLoader",
+    "DomainKnowledgeLoader",
+    "MethodologyLoader",
+    "StandardsLoader",
+    "TemplatesLoader",
+    "StrategiesLoader",
+    "KnowledgeLoaderFactory",
+    "KnowledgeLoaderManager",
 ]
